3291b4c41d3f9275c51b531512d8d3a04d728b59
J-Van02 <johnivan558@gmail.com>
2024-08-28 11:26:51 +0800

    Update product.java

--- src/firstapp/product.java
+++ src/firstapp/product.java
@@ -2,5 +2,19 @@
 package firstapp;
 
 public class product {
+
+public static void main(String[] args) {
+       
+      grade grd = new grade ();
+      grd.getGrades();
+        
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "Product ID", "Name", "Price", "Status", "TEP");
+        product pr = new product();
+        pr.addprod(1011, "Soap", 20, 10, 200);
+        pr.viewprod();
+        
+        product pr1 = new product();
+        pr1.addprod(1012, "Cards", 50, 0, 0);
+        pr1.viewprod();
     
 }
