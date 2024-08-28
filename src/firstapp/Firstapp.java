e21fefbfdcfececdaa091436c4c9b9b4a11a417b
J-Van02 <johnivan558@gmail.com>
2024-08-28 11:27:44 +0800

    Update Firstapp.java

--- src/firstapp/Firstapp.java
+++ src/firstapp/Firstapp.java
@@ -7,7 +7,15 @@ public class Firstapp {
        
       grade grd = new grade ();
       grd.getGrades();
+
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "Product ID", "Name", "Price", "Status", "TEP");
+        product pr = new product();
+        pr.addprod(1011, "Soap", 20, 10, 200);
+        pr.viewprod();
         
+        product pr1 = new product();
+        pr1.addprod(1012, "Cards", 50, 0, 0);
+        pr1.viewprod();
         
         
     }
