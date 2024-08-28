e7fed41451960a254b57b3f405661a225fc85c53
J-Van02 <johnivan558@gmail.com>
2024-08-28 10:57:30 +0800

    first time

---
+++ src/firstapp/Firstapp.java
@@ -0,0 +1,15 @@
+
+package firstapp;
+
+public class Firstapp {
+
+    public static void main(String[] args) {
+       
+      grade grd = new grade ();
+      grd.getGrades();
+        
+        
+        
+    }
+    
+}
---
+++ src/firstapp/product.java
@@ -0,0 +1,6 @@
+
+package firstapp;
+
+public class product {
+    
+}
